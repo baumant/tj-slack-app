64f23b4486432baf8feb9aa400889e0a4f1f5b3d
Timothy Bauman <timothy.bauman@wpromote.com>
2021-01-18 15:05:46 -0800

    recc function update

--- app.js
+++ app.js
@@ -55,10 +55,15 @@ app.message(/what’*'*s good TJ/i, async ({message, say }) => {
     console.log('grabbed items from database');
     itemNum = Math.floor(Math.random() * (res.rows.length - 1));
     const suggestedItem = res.rows[itemNum];
+    console.log(suggestedItem);
 
+    await say({
+      text: `have you tried the ${suggestedItem.item_title}? https://traderjoes.com${suggestedItem.item_url}`
+    })
   } catch (err) {
     console.log(err.stack);
   }
+
   // const getItem = await axios.get('https://api.apify.com/v2/datasets/AvdATgp5GXh8DIXar/items?token='+ process.env.APIFY_TOKEN)
   // .then(res => {
   //   itemNum = Math.floor(Math.random() * 9);
@@ -67,10 +72,6 @@ app.message(/what’*'*s good TJ/i, async ({message, say }) => {
 
   //   return {title: itemTitle, url: itemUrl};
   // });
-
-  await say({
-    text: `have you tried the ${suggestedItem.item_title}? https://traderjoes.com${suggestedItem.item_url}`
-  })
 
 });
 
