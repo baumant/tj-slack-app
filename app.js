cac4628581a9394d558a60b1de7b04003f7d35e0
Timothy Bauman <timothy.bauman@wpromote.com>
2021-02-03 16:53:36 -0800

    query test

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const app = new App({
       if (installQuery.teamId !== undefined) {
         // single team app installation lookup
         try {
-          const res = await db.query('SELECT installation_id FROM auth WHERE installation_id = t8dspj45q')
+          const res = await db.query('SELECT * FROM auth');
           console.log(res);
           return res;
         } catch (err) {
