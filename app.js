15c3763f0e04087a8da878797cec32be9858448c
Timothy Bauman <timothy.bauman@wpromote.com>
2021-02-03 17:54:35 -0800

    fetchteam

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const fetchTeam = async (teamId) => {
   try {
     const rows = await db.query("SELECT installation FROM slack_tokens WHERE teamid = '" + teamId + "'");
     console.log(rows[0]);
-    return JSON.parse(rows[0].installation.toString());
+    // return JSON.parse(rows[0].installation.toString());
   } catch (e) {
     console.log(e);
   }
