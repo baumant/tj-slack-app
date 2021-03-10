80079c3a686c9b6f90addca5e1665a44fd0c55cf
Timothy Bauman <timothy.bauman@wpromote.com>
2021-03-10 15:25:17 -0800

    personalize recommendation from slash command

--- app.js
+++ app.js
@@ -361,7 +361,7 @@ app.command('/tj', async ({ command, ack, say, context }) => {
             "type": "section",
             "text": {
               "type": "plain_text",
-              "text": `have you tried ${suggestedItem.item_title}?? Check it out:`,
+              "text": `@${command.user_name} have you tried ${suggestedItem.item_title}?? Check it out:`,
               "emoji": true
             }
           },
